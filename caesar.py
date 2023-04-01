7a549d3c5aa2a51e6c13a352b56d0dccbdd99acc
Alberto Madrazo <betomadrazo@gmail.com>
2023-04-01 07:53:42 -0600

    Un código basura para la locura.

--- caesar.py
+++ caesar.py
@@ -13,4 +13,16 @@ def watch(number, mod):
 def long_welcome():
     print("Bienvenido al palacio de Caesar, competencia de la casa de Toño.")
 
-long_welcome()
+lista = [3, 5, 9, 20]
+max_value = max(lista)
+
+def quiero_que_el_maximo_valor_sea(valor, lista):
+    maximo = max(lista)
+    if maximo == valor:
+        return f"{valor} si es el máximo en la lista"
+    return f"{valor} no es el máximo valor en la lista"
+
+mis_campeones = [6, 2, 4, 8, 5]
+mi_preferido = 20
+es_maximo = quiero_que_el_maximo_valor_sea(mi_preferido, mis_campeones)
+print(es_maximo)
