4a766a66e1060d1f6350485f0296d5a849db7a76
Alberto Madrazo <alberto.madrazo@coppel.com>
2023-04-01 07:28:34 -0600

    Primer commit.

---
+++ caesar.py
@@ -0,0 +1,18 @@
+BASE = 65
+
+def caesar_cipher(message, key):
+    key_as_num = convert_char_to_number(key)
+    print(key_as_num)
+
+def convert_char_to_number(key):
+    return ord(key) - BASE
+
+def watch(number, mod):
+    return number % mod
+
+def welcome():
+    print("Bienvenido a la casa de Caesar.")
+
+
+
+welcome()
